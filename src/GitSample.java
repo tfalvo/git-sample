19dca6504b16ccead459dc0a5419f50d29ae1ed6
tfalvo <tfalvo@tfalvo-laptop>
2013-05-02 11:37:48 +0200

    Initial import...

---
+++ src/GitSample.java
@@ -0,0 +1,14 @@
+
+public class GitSample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// Démarrage
+		System.out.println("Démarrage de l'application.");
+
+	}
+
+}
